7a28a27142755575997a9f3177388cd7bc750bc2
zhujunxi <531578783@qq.com>
2019-09-18 15:45:49 +0800

    feat: to adapt faas model

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const superagent = require("superagent"); // 发送网络请求获取DOM
 const cheerio = require("cheerio"); // 获取DOM节点
 const moment = require("moment")
 
+exports.main_handler = function(req, res) {
+    getOneContent().then(result => {
+        // res.writeHead(200, { 'Content-Type': 'text/html' })
+        // res.send(JSON.stringify(result))
+        res.end(JSON.stringify(result))
+    })
+}
+
 function getOneContent() {
     return new Promise(function(resolve, reject) {
         let min = 1800
@@ -9,25 +17,29 @@ function getOneContent() {
         let target = Math.floor(Math.random() * (max-min+1) +min); // 获取随机日期
         superagent.get(`http://wufazhuce.com/one/${target}`).end(function(err, res) {
             if(err) {
-                console.log(err)
+                let errData = {
+                    code: 12132,
+                    message: JSON.stringify(err)
+                }
+                reject(errData)
             }
 
             let $ = cheerio.load(res.text);
 
             let selectItem = $("#main-container .tab-content")[0];
 
-            let data = {
-                id:target,
-                imgUrl: $(selectItem).find('.one-imagen img').attr("src"),
-                tag: $(selectItem).find('.one-imagen-footer .one-imagen-leyenda').text().replace(/(^\s*)|(\s*$)/g,""),
-                content:$(selectItem).find('.one-cita-wrapper .one-cita').text().replace(/(^\s*)|(\s*$)/g, "")
+            let response = {
+                code: 0,
+                data: {
+                    id:target,
+                    imgUrl: $(selectItem).find('.one-imagen img').attr("src"),
+                    tag: $(selectItem).find('.one-imagen-footer .one-imagen-leyenda').text().replace(/(^\s*)|(\s*$)/g,""),
+                    content:$(selectItem).find('.one-cita-wrapper .one-cita').text().replace(/(^\s*)|(\s*$)/g, "")
+                },
+                message: ""
             }
 
-            resolve(data)
+            resolve(response)
         })
     })
 }
-
-getOneContent().then(res => {
-    console.log(res)
-})
